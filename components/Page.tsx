058b0296e8757421030a5d74eb6da68a1789b46e
SafeDuck <schopra579@gmail.com>
2022-12-24 22:34:57 -0800

    Changed title to 2023

--- components/Page.tsx
+++ components/Page.tsx
@@ -4,7 +4,7 @@ export default function Page({ title = '', children }) {
   return (
     <main className='flex flex-col justify-center items-center w-full min-h-screen'>
       <Head>
-        <title>BioHack 2022 { title && title !== '' && ('| ' + title) }</title>
+        <title>BioHack 2023 { title && title !== '' && ('| ' + title) }</title>
       </Head>
       <section className='flex flex-col w-full justify-center items-center'>
         {children}
